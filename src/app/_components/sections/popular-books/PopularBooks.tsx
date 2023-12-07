b7e6b82b0dd08fd4d23bcc39c398f8a1158c7f1e
Marek Matejovic <me@devmaroy.com>
2023-12-07 16:13:12 +0100

    [TMP-41] Implemented popular books & genre books error handling

--- src/app/_components/sections/popular-books/PopularBooks.tsx
+++ src/app/_components/sections/popular-books/PopularBooks.tsx
@@ -1,56 +1,50 @@
 import SectionHeader from "@/app/_components/ui/custom/SectionHeader";
-import PopularBook from "@/app/_components/sections/popular-books/PopularBook";
-import Image from "next/image";
+import PopularBooksContent from "@/app/_components/sections/popular-books/PopularBooksContent";
 import { api } from "@/trpc/server";
+import ErrorMessage from "@/app/_components/ui/custom/ErrorMessage";
 
 const PopularBooks = async () => {
-  const popularBooks = await api.book.getPopularBooks.query({});
+  let popularBooks;
+  let error;
 
-  return (
-    <section className="z-99 relative mt-96p lg:mt-120p">
-      <div className="container">
-        <SectionHeader
-          heading="Popular Books"
-          content="Let’s see what are the most popular books."
-          decorationImage="/popular-books-shape.svg"
-          decorationClassName="right-[-4.2rem] top-[-2.6rem] h-[6.3125rem] w-[6.3125rem] md:right-[-11.8rem] md:top-[-6rem] md:h-[14.375rem] md:w-[14.375rem]"
-        />
+  try {
+    popularBooks = await api.book.getPopularBooks.query({});
+  } catch (err) {
+    error = err;
+    console.error("Error fetching popular books:", error);
+  }
 
-        <div className="mt-32p grid grid-cols-fluid-fill-18-5 gap-32p lg:gap-y-40p">
-          {popularBooks.map(
-            ({
-              id,
-              slug,
-              coverImageUrl,
-              averageRating,
-              genres,
-              title,
-              teaser,
-            }) => (
-              <PopularBook
-                key={id}
-                id={id}
-                slug={slug}
-                coverImageUrl={coverImageUrl}
-                averageRating={averageRating}
-                genres={genres}
-                title={title}
-                description={teaser}
-              />
-            ),
-          )}
+  // Check if we have some error.
+  if (error) {
+    // Return an error message or a component to indicate the error
+    return (
+      <section className="z-99 relative mt-96p lg:mt-120p">
+        <div className="container">
+          <ErrorMessage />
         </div>
+      </section>
+    );
+  }
 
-        <span className="absolute bottom-[32rem] right-[60%] -z-50 h-[867px] w-[885px] md:bottom-[2rem] md:right-[80%] xl:hidden">
-          <Image
-            src="/popular-books-shape-01.svg"
-            fill
-            alt="Decoration shapes"
+  // Check if there are any books to display
+  if (popularBooks && popularBooks.length > 0) {
+    return (
+      <section className="z-99 relative mt-96p lg:mt-120p">
+        <div className="container">
+          <SectionHeader
+            heading="Popular Books"
+            content="Let’s see what are the most popular books."
+            decorationImage="/popular-books-shape.svg"
+            decorationClassName="right-[-4.2rem] top-[-2.6rem] h-[6.3125rem] w-[6.3125rem] md:right-[-11.8rem] md:top-[-6rem] md:h-[14.375rem] md:w-[14.375rem]"
           />
-        </span>
-      </div>
-    </section>
-  );
+
+          <PopularBooksContent popularBooks={popularBooks} />
+        </div>
+      </section>
+    );
+  }
+
+  return null;
 };
 
 export default PopularBooks;
---
+++ src/app/_components/sections/popular-books/PopularBooksContent.tsx
@@ -0,0 +1,57 @@
+import PopularBook from "@/app/_components/sections/popular-books/PopularBook";
+import { Button } from "@/app/_components/ui/button";
+import { type Book } from "@/types/types";
+import { ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface PopularBooksContent {
+  popularBooks: Book[];
+}
+
+const PopularBooksContent = ({ popularBooks }: PopularBooksContent) => {
+  return (
+    <div>
+      <div className="mt-32p grid grid-cols-fluid-fill-18-5 gap-32p lg:gap-y-40p">
+        {popularBooks.map(
+          ({
+            id,
+            slug,
+            coverImageUrl,
+            averageRating,
+            genres,
+            title,
+            teaser,
+          }) => (
+            <PopularBook
+              key={id}
+              id={id}
+              slug={slug}
+              coverImageUrl={coverImageUrl}
+              averageRating={averageRating}
+              genres={genres}
+              title={title}
+              teaser={teaser}
+            />
+          ),
+        )}
+      </div>
+
+      <span className="absolute bottom-[32rem] right-[60%] -z-50 h-[867px] w-[885px] md:bottom-[2rem] md:right-[80%] xl:hidden">
+        <Image src="/popular-books-shape-01.svg" fill alt="Decoration shapes" />
+      </span>
+
+      <div className="mt-24p text-right md:mt-32p">
+        <Button
+          variant="link"
+          className="p-0 !text-16p font-bold leading-1.5 text-white"
+        >
+          <Link href="/discover">Discover More Books</Link>
+          <ChevronRightIcon className="ml-8p h-4 w-4" strokeWidth={3} />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PopularBooksContent;
